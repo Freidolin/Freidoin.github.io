87ce46ef4053377e789ae5343d1e635191fd3ca2
Freidolin <yalovega.9@mail.ru>
2023-11-14 17:05:00 +0300

    1
    файлы запушены

---
+++ diary/Jopen'/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Сайтище от Яловега</title>
+</head>
+<body>
+    <h1>Список моих работ:</h1>
+    <h1>
+        <li><a href="site_of_tech/Jopen'.php">Текст</a></li>
+        <li></li>
+        <li></li>
+        <li></li>
+    </h1>
+</body>
+</html>
